c76f211135fc354983c5e5c8bbf8a1bbdbd3454f
CibileJC <cibile.cyriac@jci.com>
2022-08-10 14:31:41 +0100

    Timing Set Comprehension and Plotting Categories

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -15,7 +15,7 @@ def main():
         if not (a.issubset(b) and b.issubset(a)):
             print('Sets are NOT equal by subset test')
 
-    print(timeit.timeit(stmt="expenses.categorize_for_loop()", setup='''
+    print(timeit.timeit(stmt="expenses.categorize_set_comprehension()", setup='''
 from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
